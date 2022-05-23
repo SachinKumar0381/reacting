00d4d4b7501c313456873da97ab689e1211559a8
SachinKumar0381 <sachinkuom0591998@gmail.com>
2022-05-23 17:06:14 +0530

    first

--- src/components/TaskApp.jsx
+++ src/components/TaskApp.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 
 import styles from "./taskApp.module.css";
-
+import Header from "./TaskHeader/TaskHeader";
+import AddTask from "./AddTask/AddTask";
+import Tasks from "./Tasks/Tasks";
 const TaskApp = () => {
   // NOTE: do not delete `data-cy` key value pair
   return (
     <div data-cy="task-app" className={styles.taskApp}>
-      {/* Header */}
-      {/* Add Task */}
-      {/* Tasks */}
-    </div>
+      <Header/>
+      <AddTask/>
+      <Tasks/>
+         </div>
   );
 };
 
